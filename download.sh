7a420f479ace385d571caead1c27754cd467a9f6
azuna <32482525+monster119120@users.noreply.github.com>
2023-02-22 20:37:59 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,5 +4,14 @@
 python3 -m pip install -r requirements.txt
 
 # Example commands to downaload from all 1fps images and convert them into hdf5.
-python3 download.py --view "all" --group "all" --split "all" --framerate "[images]" ./shift_dataset/
-python3 -m shift_dev.io.to_hdf5 "./shift_dataset/**/*.zip" --zip -j 4
+# python3 download.py --view "all" --group "all" --split "all" --framerate "[images]" ./shift_dataset/
+# python3 -m shift_dev.io.to_hdf5 "./shift_dataset/**/*.zip" --zip -j 4
+
+
+python download.py --view  "[front,]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "continuous/100x" ./SHIFT_dataset              
+python download.py --view  "[front,]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "continuous/10x" ./SHIFT_dataset     
+python download.py --view  "[front,]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "continuous/1x" ./SHIFT_dataset  
+
+
+
+python download.py --view  "[front,]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "discrete" ./SHIFT_dataset      
