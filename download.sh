94d9f637f6c45e8357a1ea96ab5bfea087d8e0c4
azuna <32482525+monster119120@users.noreply.github.com>
2023-02-27 10:47:00 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,25 +2,22 @@
 
 # Install dependency
 python3 -m pip install -r requirements.txt
-pip install torchshow
+# pip install torchshow
 
 # Example commands to downaload from all 1fps images and convert them into hdf5.
 # python3 download.py --view "all" --group "all" --split "all" --framerate "[images]" ./shift_dataset/
 # python3 -m shift_dev.io.to_hdf5 "./shift_dataset/**/*.zip" --zip -j 4
 
 
-# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "continuous/100x" ./SHIFT_dataset              
-# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "continuous/10x" ./SHIFT_dataset     
-# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val,]"  --framerate "[images, videos]"   --shift "continuous/1x" ./SHIFT_dataset  
-
-
-
-python download.py --view  "[front]"   --group "[img, det_2d, seq]"  --split "[val]"  --framerate "[images]"   --shift "discrete" ./SHIFT_dataset      
+python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[train,val]"  --framerate "[images]"   --shift "discrete" ./SHIFT_dataset      
 unzip ./SHIFT_dataset/discrete/images/val/front/img.zip -d ./SHIFT_dataset/discrete/images/val/front
-# tar -xvf ./SHIFT_dataset/discrete/videos/val/front/img.tar -C ./SHIFT_dataset/discrete/videos/val/front/
+unzip ./SHIFT_dataset/discrete/images/train/front/img.zip -d ./SHIFT_dataset/discrete/images/train/front
 
 
 
-python download.py --view  "[front]"   --group "[img, det_2d, seq]"  --split "[val]"  --framerate "[images]"   --shift "continuous/100x" ./SHIFT_dataset      
+python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/100x" ./SHIFT_dataset              
+python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/10x" ./SHIFT_dataset     
+python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/1x" ./SHIFT_dataset  
 unzip ./SHIFT_dataset/continuous/images/100x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/100x/val/front/
-# tar -xvf ./SHIFT_dataset/continuous/videos/100x/val/front/img.tar -C ./SHIFT_dataset/continuous/videos/100x/val/front/
+unzip ./SHIFT_dataset/continuous/images/10x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/10x/val/front/
+unzip ./SHIFT_dataset/continuous/images/1x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/1x/val/front/
