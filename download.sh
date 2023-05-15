bae241e76cf5fcc54732fa5aa297b05a46ef65b8
azuna <1452564596@qq.com>
2023-05-15 10:39:54 +0800

    Update download.sh
    change download.sh, only download 1x continous data.

--- download.sh
+++ download.sh
@@ -9,15 +9,21 @@ python3 -m pip install -r requirements.txt
 # python3 -m shift_dev.io.to_hdf5 "./shift_dataset/**/*.zip" --zip -j 4
 
 
-python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[train,val]"  --framerate "[images]"   --shift "discrete" ./SHIFT_dataset      
-unzip ./SHIFT_dataset/discrete/images/val/front/img.zip -d ./SHIFT_dataset/discrete/images/val/front
-unzip ./SHIFT_dataset/discrete/images/train/front/img.zip -d ./SHIFT_dataset/discrete/images/train/front
+# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[train,val]"  --framerate "[images]"   --shift "discrete" ./SHIFT_dataset      
+# unzip ./SHIFT_dataset/discrete/images/val/front/img.zip -d ./SHIFT_dataset/discrete/images/val/front
+# unzip ./SHIFT_dataset/discrete/images/train/front/img.zip -d ./SHIFT_dataset/discrete/images/train/front
 
 
 
-python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/100x" ./SHIFT_dataset              
-python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/10x" ./SHIFT_dataset     
-python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/1x" ./SHIFT_dataset  
-unzip ./SHIFT_dataset/continuous/images/100x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/100x/val/front/
-unzip ./SHIFT_dataset/continuous/images/10x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/10x/val/front/
+# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/100x" ./SHIFT_dataset              
+# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/10x" ./SHIFT_dataset     
+# python download.py --view  "[front]"   --group "[img, det_2d]"  --split "[val]"  --framerate "[images]"   --shift "continuous/1x" ./SHIFT_dataset  
+# unzip ./SHIFT_dataset/continuous/images/100x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/100x/val/front/
+# unzip ./SHIFT_dataset/continuous/images/10x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/10x/val/front/
+# unzip ./SHIFT_dataset/continuous/images/1x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/1x/val/front/
+
+
+
+python download.py --view  "[front]"   --group "[img, det_2d, det_insseg_2d]"  --split "[train, val]"  --framerate "[images]"   --shift "continuous/1x" ./SHIFT_dataset  
 unzip ./SHIFT_dataset/continuous/images/1x/val/front/img.zip -d ./SHIFT_dataset/continuous/images/1x/val/front/
+unzip ./SHIFT_dataset/continuous/images/1x/train/front/img.zip -d ./SHIFT_dataset/continuous/images/1x/train/front/
